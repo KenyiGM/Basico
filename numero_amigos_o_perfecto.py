38fb5613504ea5c7b879c5ae865c1ad95a8bf8a5
KenyiGM <91709827+KenyiGM@users.noreply.github.com>
2021-10-28 01:37:05 -0500

    Add files via upload

---
+++ numero_amigos_o_perfecto.py
@@ -0,0 +1,20 @@
+#Este programa intenta encontrar todos los numeros Amigos o Perfectos
+def main():
+    numero = 0
+    while numero >= 0:
+        numero += 1
+        sumx = 0
+        sumy = 0
+        for x in range(1,numero):
+            if numero % x == 0:
+                sumx +=x
+        for y in range(1,sumx):
+            if sumx % y == 0:
+                sumy += y
+        if sumy == sumx:
+            print(numero,"Es numero Perfecto")
+        elif sumy == numero:
+            print(numero,"Es numero Amigo")
+
+if __name__ == "__main__":
+    main()
